dd62d7547f572942472b15e35df465ea48172c08
jojoner16 <laybats.jonathan@orange.fr>
2023-10-27 18:43:52 +0200

    début de transformation du html en react

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../components/header/Header';
+import Home from '../pages/home/Home';
+import Footer from '../components/footer/Footer';
+import SignIn from '../pages/sign-in/SignIn';
+
+const App = () => {
+  // useEffect permet de modifier le titre de la page
+  useEffect(() => {
+    document.title = 'Argent Bank';
+  }, []);
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
