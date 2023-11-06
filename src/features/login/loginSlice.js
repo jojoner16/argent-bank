33729caab7e58c9944ab711b007a12672e71a50f
jojoner16 <laybats.jonathan@orange.fr>
2023-11-06 16:14:40 +0100

    créations des pages login, profil et loginSlice avec interaction vers le back-end et mise en forme css

---
+++ src/features/login/loginSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const getTokenFromLocalStorage = () => {
+  return localStorage.getItem('token') || null;
+};
+
+const initialState = {
+  token: getTokenFromLocalStorage(),
+  dataUser: null,
+};
+
+export const loginSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setSignIn(state, { payload }) {
+      state.token = payload.dataUser.body?.token;
+      state.dataUser = payload.dataUser; // Stocke les données de l'utilisateur lors de la connexion
+    },
+    setSignOut: (state) => {
+      state.token = null;
+      state.dataUser = null; // Réinitialise les données de l'utilisateur lors de la déconnexion
+    },
+  },
+});
+
+export const { setSignIn } = loginSlice.actions;
+export const { setSignOut } = loginSlice.actions;
+
+export default loginSlice.reducer;
---
+++ src/pages/login/Login.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import '../../styles/pages/Login.css';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setSignIn } from '../../features/login/loginSlice';
+
+function SignIn() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          accept: 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      if (response.status === 200) {
+        const dataUser = await response.json();
+        dispatch(setSignIn({ dataUser }));
+        localStorage.setItem('token', dataUser.body?.token);
+
+        if (rememberMe) {
+          localStorage.setItem('rememberMe', 'true');
+          localStorage.setItem('email', email);
+        } else {
+          localStorage.removeItem('email');
+        }
+
+        navigate('/profil');
+      } else {
+        localStorage.removeItem('token');
+        setErrorMsg('Erreur d’identifiant ou de mot de passe');
+        console.log(
+          'Connexion Impossible : Erreur Identifiant ou Mot de passe'
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="input-remember">
+            <input
+              type="checkbox"
+              id="remember-me"
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button onClick={handleLogin} className="sign-in-button">
+            Sign In
+          </button>
+          <div className="error-msg">{errorMsg}</div>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default SignIn;
